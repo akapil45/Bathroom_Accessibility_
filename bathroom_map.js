312c0f21ac544e1b4e7725d9dbb137aaf36463f3
akapil45 <akapil45@users.noreply.github.com>
2017-08-02 14:33:15 -0700

    Update bathroom_map.js

--- bathroom_map.js
+++ bathroom_map.js
@@ -2,6 +2,7 @@
    var x = document.getElementById("demo");
    var latitude1 = 0;
    var longitude1 = 0;
+   getInitialLocation();
    function setLocation() {
        if(!isNaN(document.getElementById("userInput").value) && !isNaN(document.getElementById("userInput2").value)) {
          latitude1 = parseInt(document.getElementById("userInput").value);
@@ -33,19 +34,23 @@ var longitudeArray = [];
 var nameArray = [];
 
   function listOut() {
+    var out = "<dir><h4>List of available bathrooms</h4>";
     for(var i = 0; i < bathrooms.length; i++) {
       var lat1 = bathrooms[i].latitude;
       var long1 = bathrooms[i].longitude;
       var name1 = bathrooms[i].name;
       var count = i + 1;
+        out += count + ".  " + name1 +"   :   " + lat1 + " , "+long1+ '</a><br>';
         latitudeArray[latitudeArray.length] = lat1;
         longitudeArray[longitudeArray.length] = long1;
         nameArray[nameArray.length] = name1;
     }
+    document.getElementById("id01").innerHTML = out;
   }
 
 function initMap() {
     listOut();
+    alert("Hello");
     var uluru = {lat:latitude1, lng:longitude1};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
@@ -57,7 +62,7 @@ function initMap() {
         icon: 'blue_markerA.png'
       });
     var infoWindow = new google.maps.InfoWindow({
-      content: 'Your location'
+      content: '<h1>Your location: </h1>'
     });
     marker.addListener('click', function() {
       infoWindow.open(map, marker);
@@ -72,12 +77,20 @@ function initMap() {
       });
     }
 
+    for (var s = 0; s < bathrooms.length; s++) {
+      var location = bathrooms[s].name;
+      var street = bathrooms[s].street;
+      var city = bathrooms[s].city;
+      var state = bathrooms[s].state;
+      //alert(location + " " + street + " " + city + " " + state);
+      var infoWindow = new google.maps.InfoWindow({
+          content: location
+   });
+   var x = markers[0];
+      x.addListener('click', function(){
+        infoWindow.open(map, x);
+     });
 
-          /*var infoWindow = new google.maps.InfoWindow({
-            content: nameArray[0]
-          });
-          markers[0].addListener('click', function() {
-            infoWindow.open(map, markers[0]);
-          });*/
+  }
 
 }
