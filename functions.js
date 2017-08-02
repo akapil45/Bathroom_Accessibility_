160e701beb64bf76d72e80dc3c903ad9994dab0f
michellehuang247 <huangmichelle247@gmail.com>
2017-08-02 11:32:44 -0700

    added listout function

--- functions.js
+++ functions.js
@@ -1,1 +1,9 @@
 //code functions to be loaded in the html doc here
+function listout(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += i + ".  "+arr[i].name+"_______"+arr[i].latitude+" , "+arr[i].longitude+" --------------- "+arr[i].unisex+'<br>';
+        }
+    document.getElementById("id01").innerHTML = out;
+}
