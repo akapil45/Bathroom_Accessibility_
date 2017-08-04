105e91b4753148dd5b5783ddd342cb7812ef1c13
brennachen <brenna.chen@gmail.com>
2017-08-04 15:30:55 -0700

    Update bathroom_map.js

--- bathroom_map.js
+++ bathroom_map.js
@@ -46,7 +46,7 @@ function initMap(bathroomArray) {
     listOut();
     var uluru = {lat:latitude1, lng:longitude1};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
+        zoom: 8,
         center: uluru
     });
     var marker = new google.maps.Marker({
