f5c28f48ae1b7506e18783c94d00f888eb2e0342
brennachen <brenna.chen@gmail.com>
2017-08-04 13:10:39 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,90 +3,94 @@ function listout(arr) {
     var output = "";
     var i;
     for(i = 0; i < arr.length; i++) {
-        output += i + ".  "+arr[i].name+"_______"+arr[i].latitude+" , "+arr[i].longitude+ " ---  whole list --- "+'<br>';
+        //output += "           "+arr[i].name+", latitude: "+arr[i].latitude+" longitude: "+arr[i].longitude + '<br>';
+        output += arr[i].name+"<br>----Latitude: "+arr[i].latitude+
+                   " , Longitude: "+arr[i].longitude+"<br>----Unisex: "+arr[i].unisex +
+                  ", Wheelchair-accessible: "+arr[i].accessible + ", Changing tables available: " +
+                  arr[i].changing_table + "<br><br>";
         }
     document.getElementById("id01").innerHTML = output;
 }
 
-//shows only unisex bathrooms
-function showUnisex(arr) {
+//prints only unisex bathrooms
+function printUnisex(arr) {
   var output = "";
   var i;
   for (i = 0; i < arr.length; i++) {
     if (arr[i].unisex == true) {
-      output += i+".  "+arr[i].name+" --- unisex"+'<br>';
+      output +="           "+arr[i].name+" --- unisex"+'<br>';
     }
   document.getElementById("id01").innerHTML = output;
   }
 }
 
-//shows only accessible bathrooms
-function showAccessible(arr) {
+//prints only accessible bathrooms
+function printAccessible(arr) {
   var output = "";
   var i;
   for (i = 0; i < arr.length; i++) {
     if (arr[i].accessible == true) {
-      output += i+".  "+arr[i].name+"--- accessible"+'<br>';
+      output +="           "+arr[i].name+"--- accessible"+'<br>';
     }
   document.getElementById("id01").innerHTML = output;
   }
 }
 
-//shows only changing table bathrooms
-function showChangingTable(arr) {
+//prints only changing table bathrooms
+function printChangingTable(arr) {
   var output = "";
   var i;
   for (i = 0; i < arr.length; i++) {
     if (arr[i].changing_table == true) {
-      output += i+".  "+arr[i].name+ "--- table"+'<br>';
+      output +="           "+arr[i].name+ "--- table"+'<br>';
     }
   document.getElementById("id01").innerHTML = output;
   }
 }
 
-//shows unisex & accessible bathrooms
-function showUnisex_Accessible(arr) {
+//prints unisex & accessible bathrooms
+function printUnisex_Accessible(arr) {
   var output = "";
   var i;
   for (i = 0; i < arr.length; i++) {
     if (arr[i].unisex == true && arr[i].accessible == true) {
-      output += i+".  "+arr[i].name+"--- unisex&accessible"+'<br>';
+      output +="           "+arr[i].name+"--- unisex&accessible"+'<br>';
     }
   document.getElementById("id01").innerHTML = output;
   }
 }
 
-//shows accessible & changing table bathrooms
-function showAccessible_ChangingTable(arr) {
+//prints accessible & changing table bathrooms
+function printAccessible_ChangingTable(arr) {
   var output = "";
   var i;
   for (i = 0; i < arr.length; i++) {
     if (arr[i].changing_table == true && arr[i].accessible == true) {
-      output += i+".  "+arr[i].name+"--- accessible&table"+'<br>';
+      output +="           "+arr[i].name+"--- accessible&table"+'<br>';
     }
   document.getElementById("id01").innerHTML = output;
   }
 }
 
-//shows unisex & changing table bathrooms
-function showUnisex_ChangingTable(arr) {
+//prints unisex & changing table bathrooms
+function printUnisex_ChangingTable(arr) {
   var output = "";
   var i;
   for (i = 0; i < arr.length; i++) {
     if (arr[i].changing_table == true && arr[i].unisex == true) {
-      output += i+".  "+arr[i].name+" --- unisex&table"+'<br>';
+      output +="           "+arr[i].name+" --- unisex&table"+'<br>';
     }
   document.getElementById("id01").innerHTML = output;
   }
 }
 
-//shows unisex & accesible & table bathrooms
-function showBathroomWithAll(arr) {
+//prints unisex & accesible & table bathrooms
+function printBathroomWithAll(arr) {
   var output = "";
   var i;
   for (i = 0; i < arr.length; i++) {
     if (arr[i].changing_table == true && arr[i].unisex == true && arr[i].accessible == true) {
-      output += i+".  "+arr[i].name+" --- has all"+'<br>';
+      output +="           "+arr[i].name+" --- has all"+'<br>';
     }
   document.getElementById("id01").innerHTML = output;
   }
